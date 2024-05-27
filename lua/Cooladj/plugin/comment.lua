ed1aa5c9a4b1cc8e0b11ff12a9502cb784b95657
cooladj <101953837+cooladj@users.noreply.github.com>
2024-05-27 15:51:32 -0400

    added changed comment keybinds to ctrl /

--- lua/Cooladj/plugin/comment.lua
+++ lua/Cooladj/plugin/comment.lua
@@ -15,5 +15,19 @@ return {
 			-- for commenting tsx, jsx, svelte, html files
 			pre_hook = ts_context_commentstring.create_pre_hook(),
 		})
+
+		-- set keybinding for commenting
+		vim.api.nvim_set_keymap(
+			"n",
+			"<C-_>",
+			'<CMD>lua require("Comment.api").toggle.linewise.current()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"v",
+			"<C-_>",
+			'<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>',
+			{ noremap = true, silent = true }
+		)
 	end,
 }
